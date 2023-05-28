058f4b0688dc9cf67a352a209f3290c46d46e6ab
6335anamika <anamikakeshrisfi8@gmail.com>
2023-05-29 00:55:06 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template,request, jsonify
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+@app.route("/" , methods=['GET', 'POST'])
+def homepage():
+    return render_template('index.html')
+
+@app.route("/e-learning")
+def e_learning():
+    return render_template('E-learning.html')
+
+@app.route("/Course")
+def Course():
+    return render_template('Course.html')
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
